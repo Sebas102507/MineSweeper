d1bd6231308fcfa102c3076eb687c495c4bce721
Juan Sebastian Vargas Torres <vargas_jsebastian@javeriana.edu.co>
2021-05-29 19:33:42 -0500

    Commit

--- src/view_Controller/Main.java
+++ src/view_Controller/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("disenoJuego.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Busca Minas");
         primaryStage.setScene(new Scene(root, 450, 550));
         primaryStage.show();
     }
